0e5a6e23d344aa328877d36cec1c8ec187b85f93
lujianwei <826254726@qq.com>
2019-06-17 10:14:29 +0800

    update: credit module in design document; add: guarantor and user entity class

---
+++ p2ploanplatform/src/main/java/com/scut/p2ploanplatform/entity/Guarantor.java
@@ -0,0 +1,9 @@
+package com.scut.p2ploanplatform.entity;
+
+/**
+ * @auther: Light
+ * @date: 2019/6/17 09:59
+ * @description:
+ */
+public class Guarantor {
+}
---
+++ p2ploanplatform/src/main/java/com/scut/p2ploanplatform/entity/User.java
@@ -0,0 +1,9 @@
+package com.scut.p2ploanplatform.entity;
+
+/**
+ * @auther: Light
+ * @date: 2019/6/17 09:54
+ * @description:
+ */
+public class User {
+}
