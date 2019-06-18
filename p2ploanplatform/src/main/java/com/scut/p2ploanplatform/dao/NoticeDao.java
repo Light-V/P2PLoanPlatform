fbd4fd4a453bf3592db0593897d7ee3222c793c4
zonghang <524435492@qq.com>
2019-06-18 10:20:48 +0800

    add notice dao and unit test

---
+++ p2ploanplatform/src/main/java/com/scut/p2ploanplatform/dao/NoticeDao.java
@@ -0,0 +1,29 @@
+package com.scut.p2ploanplatform.dao;
+
+import com.scut.p2ploanplatform.entity.Notice;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by zonghang
+ * Date 2019/6/16 10:47
+ */
+@Mapper
+@Repository
+public interface NoticeDao {
+
+    @Insert("insert into notice(user_id, content, time, status) values (#{userId}, #{content}, #{time}, #{status})")
+    @Options(useGeneratedKeys = true, keyProperty = "noticeId", keyColumn = "notice_id")
+    int insert(Notice notice);
+
+    @Select("select * from notice where user_id = #{userId}")
+    List<Notice> findByUserId(String userId);
+
+    @Select("select * from notice where user_id = #{userId} and status = #{status}")
+    List<Notice> findByUserIdAndStatus(String userId, Integer status);
+
+    @Update("update notice set status = #{status} where notice_id = #{noticeId}")
+    int updateStatus(Notice notice);
+}
