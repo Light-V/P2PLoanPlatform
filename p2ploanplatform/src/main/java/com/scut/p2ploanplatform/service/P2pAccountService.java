acdd9b567d7996e59f31dfb43ae9a7e53d6d0a53
apple <tzzveryhandsome@gmail.com>
2019-06-20 10:39:39 +0800

    Add third-party interfaces

--- p2ploanplatform/src/main/java/com/scut/p2ploanplatform/service/P2pAccountService.java
+++ p2ploanplatform/src/main/java/com/scut/p2ploanplatform/service/P2pAccountService.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 public interface P2pAccountService {
     /**
      * 查询账户余额
-     * @param userId
-     * @return balance
+     * @param userId 用户ID
+     * @return balance 账户余额
      */
     BigDecimal showBalance(String userId);
 
     /**
      * 验证账户余额是否足够交易
-     * @param payerId
-     * @param amount
+     * @param payerId 付款人ID
+     * @param amount 交易金额
      * @return 余额是否足够交易
      */
     Boolean verifyTrade(String payerId, BigDecimal amount);
