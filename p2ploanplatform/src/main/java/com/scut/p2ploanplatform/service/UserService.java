132f4de4df400d83aab90fcd601a3bfa0cbb9b07
zrh <1105652679@qq.com>
2019-06-20 10:02:20 +0800

    add usercontroller and user service

---
+++ p2ploanplatform/src/main/java/com/scut/p2ploanplatform/service/UserService.java
@@ -0,0 +1,38 @@
+package com.scut.p2ploanplatform.service;
+
+import java.sql.SQLException;
+import com.scut.p2ploanplatform.entity.User;
+
+/**
+ * @author: zrh
+ * @date: 2019/6/18
+ * @description:处理用户登录注册活动
+ */
+
+
+public interface UserService {
+    /**
+     * 添加新注册用户
+     * @param userId 用户ID
+     * @param departmentId 部门ID
+     * @param password 用户密码
+     * @param phone 用户手机号码
+     * @param idCard 身份证号码
+     * @param thirdPartyId 第三方账号
+     * @param name 用户姓名
+     * @param address 用户地址
+     * @throws SQLException SQL错误
+     * @throws IllegalArgumentException 参数错误
+     * @return 添加成功后返回1,失败返回0
+     */
+    int insertUser(String userId, int departmentId, String password, String phone, String idCard, String thirdPartyId, String name, String address)
+            throws SQLException, IllegalArgumentException;
+
+    /**
+     * 根据用户账号查找用户资料
+     * @throws SQLException SQL错误
+     * @throws IllegalArgumentException 参数错误
+     * @return 返回对应的User
+     */
+    User findUser(String userId) throws SQLException, IllegalArgumentException;
+}
---
+++ p2ploanplatform/src/test/java/com/scut/p2ploanplatform/dao/UserDaoTest.java
@@ -0,0 +1,63 @@
+package com.scut.p2ploanplatform.dao;
+
+import com.scut.p2ploanplatform.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+/**
+ * @auther: zrh
+ * @date: 2019/6/17 16:10
+ * @description:
+ */
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    private User sampleUser;
+
+    @Before
+    public void setUp() throws Exception {
+        sampleUser = new User();
+        sampleUser.setUserId("2");
+        sampleUser.setDepartmentId(2);
+        sampleUser.setPassword("123456");
+        sampleUser.setPhone("15511223344");
+        sampleUser.setThirdPartyId("123");
+        sampleUser.setIdCard("456");
+        sampleUser.setAddress("双鸭山大学");
+        sampleUser.setName("zhou");
+    }
+
+    @Test
+    @Transactional
+    public void insertUserTest() {
+        int result = insertUser(sampleUser);
+        assertEquals(1, result);
+    }
+
+    private int insertUser(User newUser) {
+        return userDao.insertUser(newUser);
+    }
+
+    @Test
+    @Transactional
+    public void findUserTest() {
+
+        // check: non-empty result
+        insertUser(sampleUser);
+        User it = userDao.findUser(sampleUser.getUserId());
+        assertEquals(sampleUser, it);
+    }
+}
