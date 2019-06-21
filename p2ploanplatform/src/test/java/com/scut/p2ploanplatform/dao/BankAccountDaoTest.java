5d1586e7b6f2972b05b502bfbec0d1893516425a
apple <tzzveryhandsome@gmail.com>
2019-06-21 17:35:04 +0800

    implement P2pAccountDaoTest and BankAccountDaoTest

--- p2ploanplatform/src/test/java/com/scut/p2ploanplatform/dao/BankAccountDaoTest.java
+++ p2ploanplatform/src/test/java/com/scut/p2ploanplatform/dao/BankAccountDaoTest.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
